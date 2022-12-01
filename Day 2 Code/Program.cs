6c161b28b63e88f0fe0651c510e1ec45fc8c27ad
Madeline <madelinekoziol2005@gmail.com>
2022-12-01 14:23:06 +0000

    Day 2 Program prepped

---
+++ Day 2 Code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_2_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
